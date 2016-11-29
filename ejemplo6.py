488f8101a447dd11da1f0c635243e2b4ef7f3bf3
camoeswasmps <carlos_aiglos@hotmail.com>
2016-11-29 18:13:29 +0100

    clases

---
+++ ejemplo6.py
@@ -0,0 +1,18 @@
+def Main():
+    for step in range(5):
+        print(step)
+
+    words = ['cat', 'bat', 'hat', 'rat', 'sat']
+
+    for word in words:
+        print(word)
+        
+        num = int(input("Please enter a positive integer: "))
+       
+
+    while True:
+        for step in range(num):
+            print("Hello")
+
+if __name__ == "__main__":
+    Main()
