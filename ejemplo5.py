11cc08384e9c5e306568cd647dc65af194e17b97
camoeswasmps <carlos_aiglos@hotmail.com>
2016-11-29 14:45:25 +0100

    class practice

---
+++ ejemplo5.py
@@ -0,0 +1,11 @@
+def getInteger(prompt):
+    result = int(input(prompt))
+    return result
+
+def Main(): 
+    print("Starting")
+    output = getInteger("Please select a bloody number \n")
+    print(output)
+
+if __name__ == "__main__":
+        Main ()
